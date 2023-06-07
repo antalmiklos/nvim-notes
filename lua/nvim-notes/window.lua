ce54b3652ec083bee044e791533683d3853183cf
Antal Miklós <antal.miklos@gtk.bme.hu>
2023-06-07 20:14:22 +0200

    upload

--- lua/nvim-notes/window.lua
+++ lua/nvim-notes/window.lua
@@ -1,21 +1,33 @@
 -- Creates an object for the module.
 local M = {}
 
+local function create_buf(name)
+  if name == nil then
+    name = "notes"
+  end
+  M.buffer = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_name(name)
+end
+
+
+
 function M.open_float()
-  local width = 120;
+  if M.buffer == nil then
+    create_buf("notes")
+  end
+  local width = 60;
   local height = 60;
   local zindex = 99;
-  local buffer = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_open_win(buffer, true, {
-  relative='win',
-  width=width,
-  height=height,
-  border="single",
-  win = 1001,
-  row = 20,
-  col = 20,
-  zindex=zindex,
-})
+  vim.api.nvim_open_win(M.buffer, true, {
+    relative='win',
+    width=width,
+    height=height,
+    border="single",
+    win = 1001,
+    row = 20,
+    col = 20,
+    zindex=zindex,
+  })
 end
 
 return M
