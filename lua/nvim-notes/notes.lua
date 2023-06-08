b45517ec653f51566b9bfee30cfa95c86dc5e476
miki <antal.miklos@gtk.bme.hu>
2023-06-08 10:53:36 +0200

    toggle

--- lua/nvim-notes/notes.lua
+++ lua/nvim-notes/notes.lua
@@ -8,6 +8,7 @@ local notes_folder = "/.notes/"
 -- creates the missing file in the provided directory/path
 function M.create_missing_file(filename)
   print(root_dir..notes_folder..filename)
+  vim.fn.mkdir(root_dir..notes_folder)
  -- local f = assert(io.open(root_dir..notes_folder..filename, "a+"))
  -- if not f then
  --   vim.fn.mkdir(root_dir..notes_folder)
