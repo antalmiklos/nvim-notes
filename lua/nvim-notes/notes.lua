716027dc512c10ffb6337de38b304d4b3db70544
miki <antal.miklos@gtk.bme.hu>
2023-06-08 10:33:36 +0200

    toggle

--- lua/nvim-notes/notes.lua
+++ lua/nvim-notes/notes.lua
@@ -3,13 +3,13 @@ local M = {}
 local root_dir = vim.fn.getcwd()
 local notes_folder = ".notes/"
 
--- local quick_notes_exist = vim.fn.filereadable(root_dir+".notes/quick.txt")
+-- local quick_notes_exist = vim.fn.filereadable(root_dir..".notes/quick.txt")
 
 -- creates the missing file in the provided directory/path
 function M.create_missing_file(filename)
-  local f = io.open(root_dir+notes_folder+filename, "w")
+  local f = io.open(root_dir..notes_folder..filename, "w")
   if f ~= nil then
-    vim.api.nvim_err_writeln("can't open file: "+root_dir+filename)
+    vim.api.nvim_err_writeln("can't open file: "..root_dir..filename)
   end
   f:write("")
   f:close()
